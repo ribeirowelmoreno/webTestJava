5a73fd0b93daaf0f94796d8624f1963adc007b57
Wellynton Ribeiro Moreno <wr.moreno@samsung.com>
2019-01-25 16:05:18 -0200

    new implementations

--- src/main/java/WebTests/TestesCases/UserInfos.java
+++ src/main/java/WebTests/TestesCases/UserInfos.java
@@ -1,7 +1,9 @@
 package WebTests.TestesCases;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UserInfos {
@@ -18,14 +20,16 @@ public class UserInfos {
         driver.get("http://www.juliodelima.com.br/taskit");
 
         //Clicar no link com text "Sign in"
+        driver.findElement(By.linkText("Sing in")).click();
 
-        //Clicar no name "login" dentro do ID "signingbox"
+        //Identificando o formulario de login"
+        WebElement signingBoxForm = driver.findElement(By.id("signingbox"));
 
         //Digitar o nome dentro do name "login"
+        signingBoxForm.findElement(By.name("login")).sendKeys("julio0001");
 
         //Clicar no campo "password" dentro de "signingbox"
-
-        //Digitar a senha no campo "password"
+        signingBoxForm.findElement(By.name("password")).sendKeys("123456");
 
         //Clicar no botao "Sign in"
 
